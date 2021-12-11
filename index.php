8294e5d80040b5fc3586dd26921c12f5dc5b2d98
hugoresende27 <Hugoresende27@gmail.com>
2021-12-11 22:43:15 +0000

    a

--- index.php
+++ index.php
@@ -5,14 +5,23 @@
 ?>
 
 <body>
-    <div class="card " style="width: 18rem;">
-        <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="card-link">Card link</a>
-            <a href="#" class="card-link">Another link</a>
-        </div>
+   
+    <div class="d-flex justify-content-center" style="margin-top: 100px;"> 
+        
+        <form>
+            <div class="form-group">
+            <label for="exampleInputEmail1">Email address</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+            </div>
+            <div class="form-group">
+            <label for="exampleInputPassword1">Password</label>
+            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+        
     </div>
+    
 
 </body>
